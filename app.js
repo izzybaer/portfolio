00a28e12954c54ccdab9aa72c8f988e31ce850d7
Izabella Arya <izzy@izzys-magic-box.local>
2017-05-10 19:32:44 -0700

    added jquery functionality but cant get elements to append

--- app.js
+++ app.js
@@ -2,10 +2,33 @@
 
 var images = [];
 
-function Project (name, title, filepath, img, description) {
-  this.name = name;
-  this.title = title;
-  this.filepath = filepath;
-  this.images = [];
-  this.description = description;
+function Project (rawDataObj) {
+  this.name = rawDataObj.name;
+  this.title = rawDataObj.title;
+  this.url = rawDataObj.url;
+  // this.images = rawDataObj.images;
+  // this.body = rawDataObj.body;
+  this.description = rawDataObj.description;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+  $newProject.addClass('draft');
+
+  $newProject.find('.byline a').html(this.name);
+  $newProject.find('h1:first').html(this.title);
+  $newProject.find('.byline a').attr('href', this.url);
+  // $newProject.find('.project-body').html(this.body);
+  $newProject.find('.project-description').html(this.description);
+  $newProject.append('<hr>');
+  return $newProject;
+};
+
+projectData.forEach(function(projectObject) {
+  images.push(new Project(projectObject));
+});
+
+images.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+});
