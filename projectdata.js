c859c91efb6d3ac9fa1d359b3ac93634ca6ce016
Izabella Arya <izzy@izzys-magic-box.local>
2017-05-11 11:15:29 -0700

    added social links, added screen shots of projects

--- projectdata.js
+++ projectdata.js
@@ -6,14 +6,14 @@ var projectData = [
     name: 'Izabella Baer',
     title: 'Guardians of the Goats',
     url: 'https://izzybaer.github.io/guardians-of-the-goats/',
-    images:'guardians-of-the-goats/img/baby-goat-wallpaper.jpg',
+    images:'img/GuardiansOfTheGoats.png',
     description: 'Guardians of the Goats was my final project for my 201 Intro to Software Development class at Code Fellows. We designed a basic website dedicated to rescuing goats, and connecting people interesting in rescuing goats, across the country. Our site allows people to become a Guardian of the Goats by adding their information and populating it to the page. Others who visit the site are then able to see the current Guardians and contact them directly. The site is also capable of allowing people who know of goats that need help or rescuing to add that goat and all of its information to the site.',
   },
   {
     name: 'Izabella Baer',
     title: 'BusMall',
-    url: 'https://izzybaer.github.io/guardians-of-the-goats/',
-    images:'guardians-of-the-goats/img/baby-goat-wallpaper.jpg',
+    url: 'https://izzybaer.github.io/bus-mall/',
+    images:'img/BusMall.png',
     description: 'Guardians of the Goats was my final project for my 201 Intro to Software Development class at Code Fellows. We designed a basic website dedicated to rescuing goats, and connecting people interesting in rescuing goats, across the country. Our site allows people to become a Guardian of the Goats by adding their information and populating it to the page. Others who visit the site are then able to see the current Guardians and contact them directly. The site is also capable of allowing people who know of goats that need help or rescuing to add that goat and all of its information to the site.',
   },
 ];
